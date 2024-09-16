e760b379ea2f682913410cece9eae57301f989ad
naumorek <38781843+naumorek@users.noreply.github.com>
2024-09-16 11:50:45 +0300

    Add files via upload

---
+++ lessons 2.py
@@ -0,0 +1,6 @@
+
+complite_DZ=12
+hour_for_DZ=1.5
+curs_name='Python'
+hour_for_1lesson=hour_for_DZ/complite_DZ
+print("Курс:"+ curs_name +","+" всего задач:"+ str(complite_DZ) +" затрачено часов:"+ str(hour_for_DZ) + " среднее время выполнения:"+ str(hour_for_1lesson) +" часа")
